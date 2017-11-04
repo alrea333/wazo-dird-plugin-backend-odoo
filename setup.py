3aefedb71cb14e8fd13ce1b9c503468454a34aed
Sylvain Boily <sboily@avencall.com>
2017-11-04 07:01:59 -0400

    rename xivo to wazo, add a min wazo version

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ from setuptools import setup
 from setuptools import find_packages
 
 setup(
-    name='xivo_dird_plugin_odoo',
+    name='wazo_dird_plugin_odoo',
     version='0.1',
 
-    description='XiVO dird plugins to search in Odoo',
+    description='Wazo dird plugins to search in Odoo',
 
     author='Alexis de Lattre',
     author_email='alexis@via.ecp.fr',
@@ -19,7 +19,7 @@ setup(
 
     entry_points={
         'xivo_dird.backends': [
-            'odoo = xivo_dird_plugin_odoo.odoo_plugin:OdooPlugin',
+            'odoo = wazo_dird_plugin_odoo.odoo_plugin:OdooPlugin',
         ],
     }
 )
