cb632be8cf2af58105aa8cb8e6f07784da8c91c3
Sylvain Boily <sylvainboilydroid@gmail.com>
2020-07-24 12:52:15 -0400

    fix version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import find_packages
 
 setup(
     name='wazo_dird_plugin_odoo',
-    version='0.2',
+    version='0.0.2',
 
     description='Wazo dird plugins to search in Odoo',
 
