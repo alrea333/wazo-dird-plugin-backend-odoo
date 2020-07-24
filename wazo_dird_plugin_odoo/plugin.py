58bfe21b021f3fd77732b3f1364cc87c87ceb862
Sylvain Boily <sylvainboilydroid@gmail.com>
2020-07-24 13:28:55 -0400

    python3 xmlrpc

--- wazo_dird_plugin_odoo/plugin.py
+++ wazo_dird_plugin_odoo/plugin.py
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>
 
-import xmlrpclib
+import xmlrpc.client as xmlrpclib
 import logging
 
 from wazo_dird import BaseSourcePlugin, make_result_class
