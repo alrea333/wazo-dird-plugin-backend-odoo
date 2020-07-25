4409f0b6cde75940573372f3f46968032fcd9b51
Sylvain Boily <sylvainboilydroid@gmail.com>
2020-07-25 11:36:24 -0400

    WIP

--- wazo_dird_plugin_odoo/ui/plugin.py
+++ wazo_dird_plugin_odoo/ui/plugin.py
@@ -1,7 +1,7 @@
 # Copyright 2020 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-from flask import render_template
+from flask import render_template,redirect
 from flask_classful import route
 
 
