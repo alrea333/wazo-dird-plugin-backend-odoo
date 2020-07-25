6bef696316c17f50a5feb4d110dbf1dc6e0e52fd
Sylvain Boily <sylvainboilydroid@gmail.com>
2020-07-25 07:50:09 -0400

    WIP UI add form

--- wazo_dird_plugin_odoo/ui/plugin.py
+++ wazo_dird_plugin_odoo/ui/plugin.py
@@ -1,12 +1,12 @@
 # Copyright 2020 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-from wazo_ui.plugins.dird_sources.plugin import dird_source as bp
+from wazo_ui.plugins.dird_source.plugin import dird_source as bp
 
-class Plugin(object):
+class OdooUi(object):
 
     def load(self, dependencies):
 
-        @bp.route('/test')
+        @bp.route('dird_source/test')
         def test_view():
             return "Hi!"
